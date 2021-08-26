fb9e7f281b31a5691d70152b5f9a4675f12b3e56
Iggi <iggijimenez@gmail.com>
2021-08-25 18:38:59 -0700

    first commit

---
+++ ProductHuntExample/FeedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedViewController.swift
+//  ProductHuntExample
+//
+//  Created by Jimenez on 8/25/21.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+//MARK: UITableViewDataSource
+extension FeedViewController: UITableViewDataSource {
+    /// Determines how many cells will be shown on the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 3
+    }
+    
+    /// Creates and configures each cell.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension FeedViewController: UITableViewDelegate {
+    //code to handle cell events goes here
+    
+}
